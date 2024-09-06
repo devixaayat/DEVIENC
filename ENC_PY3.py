8707aa89ff4827a2958c917c734918c6e73eb9d9
D3VI R9JPOOT <devixaayat@gmail.com>
2024-09-07 01:15:46 +0500

    Add files via upload

---
+++ ENC_PY3.py
@@ -0,0 +1,33 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from PY6 import DEVI
+ 
+        DEVI()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from PY3 import DEVI
+ 
+ 
+        DEVI()
+ 
+ 
+ 
---
+++ README.md
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade
+
+pkg install git
+
+pkg install python
+
+git clone https://github.com/MAHADI-143/ENC_PY3.git
+
+cd ENC_PY3
+
+python ENC_PY3.py
